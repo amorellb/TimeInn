90ec90fa0e66458e66ac052aa8edfd7d07af7a5d
Victoria <mpelaez@cifpfbmoll.eu>
2021-11-09 16:41:56 +0100

    Creation of the function to ordered news by sponsored

--- src/js/landingPage/news.js
+++ src/js/landingPage/news.js
@@ -1,34 +1,46 @@
 const newsSection = document.querySelector('.news-container');
 
-// probar for of, for in, for each
+
+
+
 export const generateNews = function (news) {
+
+    let sponsoredNews = news.filter(function(n){
+        return n.sponsored == true;
+    })
+    let notSponsoredNews = news.filter(function(n){
+        return n.sponsored == false;
+    })
+    let newArrayNews = sponsoredNews.concat(notSponsoredNews)
+    console.log(newArrayNews);
+
     return `
     <div class="news-info">
-        <img class="news-image" src="${news[0].imgURL}" alt="News Image">
-        <p class="news-title">${news[0].title}</p>
-        <p class="news-content">${news[0].content}</p>
-        <p class="news-title">${news[0].publicationDate}</p>
+        <img class="news-image" src="${newArrayNews[0].imgURL}" alt="News Image">
+        <p class="news-title">${newArrayNews[0].title}</p>
+        <p class="news-content">${newArrayNews[0].content}</p>
+        <p class="news-title">${newArrayNews[0].publicationDate}</p>
    </div>;
    
     <div class="news-info">
-        <img class="news-image" src="${news[1].imgURL}" alt="News Image">
-        <p class="news-title">${news[1].title}</p>
-        <p class="news-content">${news[1].content}</p>
-        <p class="news-title">${news[1].publicationDate}</p>
+        <img class="news-image" src="${newArrayNews[1].imgURL}" alt="News Image">
+        <p class="news-title">${newArrayNews[1].title}</p>
+        <p class="news-content">${newArrayNews[1].content}</p>
+        <p class="news-title">${newArrayNews[1].publicationDate}</p>
    </div>;
    
     <div class="news-info">
-        <img class="news-image" src="${news[2].imgURL}" alt="News Image">
-        <p class="news-title">${news[2].title}</p>
-        <p class="news-content">${news[2].content}</p>
-        <p class="news-title">${news[2].publicationDate}</p>
+        <img class="news-image" src="${newArrayNews[2].imgURL}" alt="News Image">
+        <p class="news-title">${newArrayNews[2].title}</p>
+        <p class="news-content">${newArrayNews[2].content}</p>
+        <p class="news-title">${newArrayNews[2].publicationDate}</p>
    </div>;
    
     <div class="news-info">
-        <img class="news-image" src="${news[3].imgURL}" alt="News Image">
-        <p class="news-title">${news[3].title}</p>
-        <p class="news-content">${news[3].content}</p>
-        <p class="news-title">${news[3].publicationDate}</p>
+        <img class="news-image" src="${newArrayNews[3].imgURL}" alt="News Image">
+        <p class="news-title">${newArrayNews[3].title}</p>
+        <p class="news-content">${newArrayNews[3].content}</p>
+        <p class="news-title">${newArrayNews[3].publicationDate}</p>
    </div>`;
 }
 
