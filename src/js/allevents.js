a2154a7675b366b4c72e06c976c3b16ab2ce8fbc
amorellb <amorellb@cifpfbmoll.eu>
2021-11-12 09:44:15 +0100

    feature: make the user able to add and delete events on all-events page

--- src/js/allevents.js
+++ src/js/allevents.js
@@ -1,4 +1,11 @@
 const eventsContainer = document.querySelector('.allevents-container');
+const addEventBtn = document.querySelector('.add-event-btn');
+
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const overlay = document.querySelector('.overlay');
+const addEventWindow = document.querySelector('.add-event-window');
+const addEventForm = document.querySelector('.upload');
+const uploadBtn = document.querySelector('.upload__btn');
 
 export const generateEventsMarkup = function (event) {
   return `
@@ -6,12 +13,60 @@ export const generateEventsMarkup = function (event) {
       <img src="${event.imgURL}" alt="${event.title}">
       <h2>${event.title}</h2>
       <p>${event.description}</p>
-      <div class="trash-container">
-        <i class="fas fa-trash-alt"></i>
-      </div>
+        <i class="fas fa-trash-alt trash-icon"></i>
     </div>`;
 };
 
 export const render = function (markup) {
+  if (!eventsContainer) return;
   eventsContainer.insertAdjacentHTML('beforeend', markup);
 };
+
+const toggleWindow = function () {
+  if (!overlay || !addEventWindow) return;
+  overlay.classList.toggle('hidden');
+  addEventWindow.classList.toggle('hidden');
+};
+
+const addHandlerShowForm = function () {
+  if (!addEventBtn) return;
+  addEventBtn.addEventListener('click', toggleWindow);
+};
+addHandlerShowForm();
+
+const addHandlerHideForm = function () {
+  if (!btnCloseModal) return;
+  btnCloseModal.addEventListener('click', toggleWindow);
+  overlay.addEventListener('click', toggleWindow);
+};
+addHandlerHideForm();
+
+const uploadEvent = function () {
+  if (!uploadBtn) return;
+  uploadBtn.addEventListener('click', e => {
+    e.preventDefault();
+    const formData = getFormData();
+    console.log(formData);
+    const markup = generateEventsMarkup(formData);
+    eventsContainer.insertAdjacentHTML('afterbegin', markup);
+    toggleWindow();
+  });
+};
+uploadEvent();
+
+const getFormData = function () {
+  const formData = new FormData(addEventForm);
+  if (!formData) return;
+  const data = Object.fromEntries(formData);
+  return data;
+};
+
+const deleteEvent = function () {
+  if (!eventsContainer) return;
+  eventsContainer.addEventListener('click', e => {
+    const trashIcon = e.target.closest('.trash-icon');
+    if (!trashIcon) return;
+    e.target.parentElement.outerHTML = '';
+  });
+};
+deleteEvent();
