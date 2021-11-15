7379633928fbdd7fd58a52ef7275da1621f4586a
amorellb <amorellb@cifpfbmoll.eu>
2021-11-15 12:36:15 +0100

    Fix nav icon problem and delete all html headers and footer

--- src/js/landingPage/nav.js
+++ src/js/landingPage/nav.js
@@ -1,15 +1,20 @@
 const menu = document.getElementsByClassName('nav-section')[0];
-let iconoMenu = document.getElementsByClassName('menu-icon')[0];
-let iconoClose = document.getElementsByClassName('x-close')[0];
+let iconMenu = document.getElementsByClassName('menu-icon')[0];
+let iconClose = document.getElementsByClassName('x-close')[0];
 
 const toggleMenu = () => {
   menu.classList.toggle('menu2');
   menu.style.transition = 'transform 0.5s ease-in-out';
 };
-iconoMenu.addEventListener('click', function () {
-  toggleMenu();
-});
 
-iconoClose.addEventListener('click', function () {
-  toggleMenu();
-});
+const openMenuHandler = function () {
+  if (!iconMenu) return;
+  iconMenu.addEventListener('click', toggleMenu());
+};
+openMenuHandler();
+
+const closeMenuHandler = function () {
+  if (!iconClose) return;
+  iconClose.addEventListener('click', toggleMenu());
+};
+closeMenuHandler();
