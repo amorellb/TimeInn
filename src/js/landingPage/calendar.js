36c4115f3033d42615b1c58820033d18552a36eb
Victoria <mpelaez@cifpfbmoll.eu>
2021-11-16 16:34:11 +0100

    refactor function event day calendar

--- src/js/landingPage/calendar.js
+++ src/js/landingPage/calendar.js
@@ -5,34 +5,25 @@ export const createCalendar = function () {
   for (let i = 1; i <= 35; i++) {
     if (i <= 31) {
       buttons += `<button class="calendar-btn calendar-${i}">${i}</button>`;
-    } else {
-      buttons += `<button class="calendar-btn calendar-${i}"></button>`;
+    } else if (i > 31) {
+      buttons += `<button class="calendar-btn calendar-empty"></button>`;
     }
   }
   return buttons;
 };
 
-export function addEventCalendar() {
-  if (document.querySelector('.calendar-28')) {
-    document
-      .querySelector('.calendar-28')
-      .addEventListener('click', function () {
-        window.location.assign('allevents.html');
-      });
-  }
-}
-/* export function addEventCalendar() {
-  for (let i = 1; i <= 35; i++){
-    if (document.querySelector(`.calendar-${i}`)) {
-      document
-        .querySelector(`.calendar-${i}`)
-        .addEventListener('click', function () {
-          window.location.assign('event.html');
-        });
-    }}
-  } */
-
 export const render = function (markup) {
   if (!dayContainer) return;
   dayContainer.insertAdjacentHTML('beforeend', markup);
 };
+
+export function addEventCalendar() {
+  if (!dayContainer) return;
+  dayContainer.addEventListener('click', e => {
+    const btn = e.target.closest('.calendar-btn');
+    if (!btn) return;
+    if (btn.classList.contains('calendar-empty')) return;
+    if (btn.classList.contains('calendar-28'))
+      window.location.assign('allevents.html');
+  });
+}
